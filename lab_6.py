6effe3c3e0cd94e0e57130b2bcbefc9ca1ee8cb6
k-luka <kirilldenisluka@gmail.com>
2023-03-08 10:21:09 -0500

    main() loop

---
+++ lab_6.py
@@ -0,0 +1,13 @@
+def main():
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            pass
+        elif option == 2:
+            pass
+        else:
+            break
+
+if __name__ == "__main__":
+    main()
